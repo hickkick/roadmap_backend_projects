ab68de1afe03b94f18143b314022970484273e20
Roman Oli <inthewavves@gmail.com>
2025-09-11 18:13:04 +0200

    refactor(app): change game flow

--- number-guessing_game/app.rb
+++ number-guessing_game/app.rb
@@ -1,23 +1,37 @@
 require_relative "./lib/game"
 
 puts "Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nYou have 5 chances to guess the correct number."
+print "Please enter your name: "
+player_name = gets.strip
 scores = Dashboard.new
-sleep 1
 repeats = 1
+
 loop do
-  game = Game.new
-  game.set_level
-  game.play
-  sleep 1
+  puts "\n=== Main Menu ==="
+  puts "1. Start new game"
+  puts "2. Show leaderboard"
+  puts "3. Exit\n\n"
+  print "Choose an option: "
+  choice = gets.strip
 
-  scores.add(game.to_h)
-  scores.save
+  case choice
+  when "1"
+    game = Game.new(player_name)
+    game.set_level
+    game.play
 
-  puts "Round ##{repeats} finished."
-  repeats += 1
-
-  puts "Wanna try again? (y/n)"
-  answer = gets.strip.downcase
-  break unless ["y", "yes"].include?(answer)
+    scores.add(game.to_h)
+    scores.save
+    sleep 2
+    puts "Round ##{repeats} finished."
+    repeats += 1
+    sleep 1
+  when "2"
+    puts scores.make
+  when "3"
+    puts "Goodbye, #{player_name}!"
+    break
+  else
+    puts "Invalid option. Try again."
+  end
 end
-puts scores.make
